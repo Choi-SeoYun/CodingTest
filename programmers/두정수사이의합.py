d389a6fcc67c28c825336b060d7f014d84bd8e26
Choi-SeoYun <ab349226@gmail.com>
2021-04-25 19:34:46 +0900

    두 정수 사이의 합

---
+++ programmers/두정수사이의합.py
@@ -0,0 +1,18 @@
+def solution(a, b):
+    answer = 0
+    if a > b:
+        for i in range(b, a+1):
+            answer += i
+    elif a < b:
+        for i in range(a, b+1):
+            answer += i
+    else: answer=a
+    
+    return answer
+
+
+# def solution(a, b):
+#     answer = 0
+
+#     if a > b: a, b = b, a
+#     answer = sum(range(a, b+1))
